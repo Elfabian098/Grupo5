2060cb6ffdbef176f699ce10cb5eacd3d5255850
fabia <fpintopa@ucvvirtual.edu.pe>
2024-05-10 21:08:51 -0500

    prueba

--- lib/prueba.dart
+++ lib/prueba.dart
@@ -5,3 +5,6 @@
 //Andre
 //que tal soy pisse
 //Kevin
+//Si por primera  vez entran al ID, hacer el MENU/GIT/PULL (Usar el NAVEGIETor  buscarlo),
+//luego hacer los cambios y guardar, en comit(alt + 0), marcar los cambios, despues
+//en el boton Commit and push presionar y seguir los pasos de esas ventanas emergentes
